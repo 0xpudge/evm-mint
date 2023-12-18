f572776b74f2ad1905ac08dce2f2231c42087359
Birch <birch17261@gmail.com>
2023-12-18 13:12:08 +0800

    feat: update

--- src/components/start.tsx
+++ src/components/start.tsx
@@ -32,7 +32,7 @@ export default function Start() {
     const provider = rpc
       ? new ethers.JsonRpcProvider(rpc)
       : ethers.getDefaultProvider("goerli", [
-          "https://ethereum-goerli.publicnode.com",
+          "https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
         ]);
     const wallet = new ethers.Wallet(key, provider);
     const address = await wallet.getAddress();
